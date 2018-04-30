7e1893c36f024bf8e4069e8d887f8fcde4a49886
lee-jw <35711738+lee-jw@users.noreply.github.com>
2018-04-30 15:04:09 -0400

    Add files via upload

---
+++ dataExtract.m
@@ -0,0 +1,60 @@
+%% Read info.txt file, which was extracted from https://api.trackico.io/v1/icos/, and process data to put in a dataframe
+% Read file
+text = fileread('info.txt');
+idx_titles = strfind(text,'title":"');
+projects = string([]);
+
+% Separate one big string into projects
+for i = 1:length(idx_titles)-1
+    projects = [projects text(idx_titles(i)-1:idx_titles(i+1)-5)];
+end
+
+title = string([]);
+slug = string([]);
+url = string([]);
+ico_type = string([]);
+category = string([]);
+rating = string([]);
+pre_ico_start = string([]);
+pre_ico_end = string([]);
+ico_start = string([]);
+ico_end = string([]);
+
+% Extract features
+for i = 1:length(projects)
+    project = projects{i};
+    title = [title project(strfind(project,'"title":"')+length('title')+length('":"')+1:strfind(project,',"slug"')-2)];
+    slug = [slug project(strfind(project,'"slug"')+length('slug')+length('":"')+1:strfind(project,',"url"')-2)];
+    url = [url project(strfind(project,'"url"')+length('url')+length('":"')+1:strfind(project,',"ico_type"')-2)];
+    ico_type = [ico_type project(strfind(project,'"ico_type"')+length('ico_type')+length('":"')+1:strfind(project,',"premium"')-2)];
+    category = [category project(strfind(project,'"category"')+length('category')+length('":"')+1:strfind(project,',"short_description"')-2)];
+    rating = [rating project(strfind(project,'"rating"')+length('rating')+length('":"')+1:strfind(project,',"image"')-2)];
+    pre_ico_start = [pre_ico_start project(strfind(project,'"pre_ico_start"')+length('pre_ico_start')+length('":"')+1:strfind(project,',"pre_ico_end"')-2)];
+    pre_ico_end = [pre_ico_end project(strfind(project,'"pre_ico_end"')+length('pre_ico_end')+length('":"')+1:strfind(project,',"ico_start"')-2)];
+    ico_start = [ico_start project(strfind(project,'"ico_start"')+length('ico_start')+length('":"')+1:strfind(project,',"ico_end"')-2)];
+    ico_end = [ico_end project(strfind(project,'"ico_end"')+length('ico_end')+length('":"')+1:strfind(project,',"updated_at"')-2)];
+end
+
+% Process data - inconsistencies in raw data resulted in "null" to show as "ul"
+for j = 1:length(pre_ico_start)
+    if pre_ico_start(j) == 'ul'
+        pre_ico_start(j) = 'null';
+    end
+    if pre_ico_end(j) == 'ul'
+        pre_ico_end(j) = 'null';
+    end
+    if ico_start(j) == 'ul'
+        ico_start(j) = 'null';
+    end
+    if ico_end(j) == 'ul'
+        ico_end(j) = 'null';
+    end
+end
+
+% Combine all data into matrix and export as csv file
+data = [title', slug', url', ico_type', category', rating', pre_ico_start', pre_ico_end', ico_start', ico_end'];
+fid = fopen('data.csv', 'w');
+for k = 1:length(data)
+    fprintf(fid,'%s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n',data{k,:});
+end
+fclose(fid)
